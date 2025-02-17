60df3a57fd6a30df502d198afbcabab6c04a95eb
Mauro Molin <molin.mauro@gmail.com>
2025-02-17 21:29:36 +0000

    Add load deck functionality

--- src/main/kotlin/dev/mmauro/malverio/commands/NewGameCommand.kt
+++ src/main/kotlin/dev/mmauro/malverio/commands/NewGameCommand.kt
@@ -1,61 +1,30 @@
 package dev.mmauro.malverio.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.path
-import dev.mmauro.malverio.City
 import dev.mmauro.malverio.GameLoop
 import dev.mmauro.malverio.InfectionCard
 import dev.mmauro.malverio.Timeline
+import kotlinx.serialization.json.Json
+import kotlin.io.path.createParentDirectories
+import kotlin.io.path.readText
 
-private val INFECTION_DECK = setOf(
-    InfectionCard(City.ATLANTA),
-    InfectionCard(City.BUENO_AIRES),
-    InfectionCard(City.BUENO_AIRES),
-    InfectionCard(City.CAIRO),
-    InfectionCard(City.CAIRO),
-    InfectionCard(City.CAIRO),
-    InfectionCard(City.FRANKFURT),
-    InfectionCard(City.FRANKFURT),
-    InfectionCard(City.ISTANBUL),
-    InfectionCard(City.ISTANBUL),
-    InfectionCard(City.ISTANBUL),
-    InfectionCard(City.JACKSONVILLE),
-    InfectionCard(City.JACKSONVILLE),
-    InfectionCard(City.JACKSONVILLE),
-    InfectionCard(City.LAGOS),
-    InfectionCard(City.LAGOS),
-    InfectionCard(City.LAGOS),
-    InfectionCard(City.LIMA),
-    InfectionCard(City.LONDON),
-    InfectionCard(City.LONDON),
-    InfectionCard(City.LONDON),
-    InfectionCard(City.LOS_ANGELES),
-    InfectionCard(City.NEW_YORK),
-    InfectionCard(City.NEW_YORK),
-    InfectionCard(City.NEW_YORK),
-    InfectionCard(City.PARIS),
-    InfectionCard(City.PARIS),
-    InfectionCard(City.SANTIAGO),
-    InfectionCard(City.SAO_PAULO),
-    InfectionCard(City.SAO_PAULO),
-    InfectionCard(City.SAO_PAULO),
-    InfectionCard(City.TRIPOLI),
-    InfectionCard(City.TRIPOLI),
-    InfectionCard(City.TRIPOLI),
-    InfectionCard(City.WASHINGTON),
-    InfectionCard(City.WASHINGTON),
-    InfectionCard(City.WASHINGTON, setOf(InfectionCard.Mutation.WELL_STOCKED)),
-)
+private typealias InfectionDeck = Set<InfectionCard>
 
 class NewGameCommand : CliktCommand(name = "new") {
 
+    private val infectionDeck by option().path(mustExist = true)
+        .convert { Json.decodeFromString<InfectionDeck>(it.readText()) }
+        .required()
     private val savegame by option().path(canBeDir = false, canBeSymlink = false).required()
 
     override fun run() {
+        savegame.createParentDirectories()
         GameLoop(
-            startTimeline = Timeline(infectionDeck = INFECTION_DECK),
+            startTimeline = Timeline(infectionDeck = infectionDeck),
             savegame = savegame,
         ).run()
     }
